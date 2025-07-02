bb57ae856f0353b923fbffa2816437c841a9f67b
KuLiPai <yun.svip@gmail.com>
2025-07-03 00:51:14 +0800

    重构&优化，适配安卓16 更新material库，dexkit库等 添加应用自动申请作用域

---
+++ libxposed/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "libxposed"
+
+include(":api", ":checks")
+include(":service", ":interface")
